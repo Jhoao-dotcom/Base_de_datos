96b43074aeee798e03e474e2700e00fa9d8a6983
Jhoaos <jhare24jhoao@gmail.com>
2025-09-07 22:28:43 -0500

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -5,6 +5,7 @@ create database estudiantes
 FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\mi primera vez.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
 
 use [mi primera vez];
+
 go 
 create table estudiantes (
 	id int identity(1,1) primary key,
@@ -56,5 +57,6 @@ go
 SELECT * 
 FROM Estudiantes
 WHERE carrera = 'Ingeniería' OR carrera IS NULL;
+
 
 
